c604eb8ba55c8945d204b6a2dcd1f64620e30254
KKshakya <krishnashakya7272@gmail.com>
2022-01-22 18:33:31 +0530

    Search by terms

--- src/components/NewsCards/styles.js
+++ src/components/NewsCards/styles.js
@@ -1,11 +1,21 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles({
-    container: {
-           padding:'0.5%',
-           width: '100%',
-           margin:0,
-
-    }
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    height: '50vh',
+    padding: '10%',
+    borderRadius: 20,
+    color: 'Black',
+  },
+  infoCard: {
+    display: 'flex', flexDirection: 'column', textAlign: 'center',
+  },
+  container: {
+    padding: '0 5%', width: '100%', margin: 0,
+  },
 });
-
